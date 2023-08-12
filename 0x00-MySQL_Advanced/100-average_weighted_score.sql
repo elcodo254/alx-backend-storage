db3c338be0cf07c6f588f90adf0b4c19a140acaa
elcodo254 <ellawachaga@gmail.com>
2023-08-12 21:30:59 +0000

    fix bug

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -8,7 +8,7 @@ BEGIN
     SET average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
         FROM corrections
 	INNER JOIN projects
-	ON project.id = corrections.project_id
+	ON projects.id = corrections.project_id
 	WHERE corrections.user_id = user_id)
     WHERE users.id = user_id;
 END $$
