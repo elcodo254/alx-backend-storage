b5856f4b081df26f9b42c67e28888278e8477b3c
elcod254 <ellawachaga@gmail.com>
2023-10-09 13:30:49 -0700

    task7 - Average score

---
+++ 0x00-MySQL_Advanced/7-main.sql
@@ -0,0 +1,16 @@
+Enter password: 
+id  name    average_score
+1   Bob 0
+2   Jeanne  0
+user_id project_id  score
+1   1   80
+1   2   96
+2   1   91
+2   2   73
+--
+--
+--
+--
+id  name    average_score
+1   Bob 0
+2   Jeanne  82
